adbe75fb3bf0d26c82fa528cf7d9f9fce3478826
kimsoomin <rinahee@naver.com>
2022-08-27 16:50:48 +0900

    app.js 및 index.js 수정

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+function App() {
+    return (
+        <Container>
+            App
+        </Container>
+    )
+}
+const Container = styled.div``;
+
+export default App;
